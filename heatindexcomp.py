8bc06397d53432d89f3f127c71a0cd501b3603f2
Robert Prestley <prestley@ucar.edu>
2021-01-27 13:13:07 -0700

    Copying first script to start second

---
+++ heatindexcomp.py
@@ -0,0 +1,55 @@
+# Column names and column indices to read
+columns = {'date': 0, 'time': 1, 'tempout': 2, 'windspeed': 7, 'windchill': 12}
+
+# Data types for each column (only if non-string)
+types = {'tempout': float, 'windspeed': float, 'windchill': float}
+
+# Initialize my data variable
+data = {}
+for column in columns:
+    data[column] = []
+
+# Read the data file
+filename = 'data/wxobs20170821.txt'
+
+with open(filename, 'r') as datafile:
+
+    # Read the first three lines (header)
+    for _ in range(3):
+        datafile.readline()
+
+    # Read and parse the rest of the file
+    for line in datafile:
+        split_line = line.split()
+        for column in columns:
+            i = columns[column]
+            t = types.get(column, str)
+            value = t(split_line[i])
+            data[column].append(value)
+
+
+# Compute the wind chill temperature
+def compute_windchill(temp, vel):
+    a = 35.74
+    b = 0.6215
+    c = 35.75
+    d = 0.4275
+
+    vel16 = vel**0.16
+    wci = a + (b*temp) - (c*vel16) + (d*temp*vel16)
+    return wci
+
+
+# Running the function to compute wind chill index
+windchill = []
+for temp, windspeed in zip(data['tempout'], data['windspeed']):
+    windchill.append(compute_windchill(temp, windspeed))
+
+# Output comparison of data
+print('                ORIGINAL  COMPUTED')
+print(' DATE    TIME  WINDCHILL WINDCHILL DIFFERENCE')
+print('------- ------ --------- --------- ----------')
+zip_data = zip(data['date'], data['time'], data['windchill'], windchill)
+for date, time, wc_orig, wc_comp in zip_data:
+    wc_diff = wc_orig - wc_comp
+    print(f'{date} {time:>6} {wc_orig:9.6f} {wc_comp:9.6f} {wc_diff:10.6f}')
