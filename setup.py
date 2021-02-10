d37ee4f4a44dfb32c248a757faa3e0449c214ac8
Robert Prestley <prestley@ucar.edu>
2021-02-10 13:43:36 -0700

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name="mysci",
+    version="1.0.0",
+    description="A sample package",
+    author="Robert Prestley",
+    author_email="prestley@ucar.edu",
+    packages=["mysci"],
+    install_requires=[],
+)
